28083e0c8caeb207e1cbf1882e9cd587c255ee66
osama <osamaahmedwork1@gmail.com>
2026-01-28 00:47:12 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,21 @@ import HeroSection from '@/components/homeComponents/HeroSection'
 import SecondHero from '@/components/homeComponents/SecondHero'
 import SpecialitySection from '@/components/homeComponents/SpecialitySection'
 import TopDoctorsSection from '@/components/homeComponents/TopDoctorsSection'
+import { useEffect } from 'react'
+import '@n8n/chat/style.css'
+import { createChat } from '@n8n/chat'
+import { useUser } from './store/useUser'
 
 const Home = () => {
+  const { user } = useUser()
+  useEffect(() => {
+    createChat({
+      webhookUrl: `${import.meta.env.CHATBOT_URL}`,
+      metadata: {
+        userID: user?._id,
+      },
+    })
+  }, [])
   return (
     <div className="flex flex-col gap-20">
       <HeroSection />
